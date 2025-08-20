9550316d81f9f5969520331a31f70635f0259578
fracappa <francesco.cappa95@gmail.com>
2025-08-20 19:03:27 +0200

    adjusting fibonacci function

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,11 +1,18 @@
 package fibonacci
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func ParseIndex(i string) (string, error) {
-	if i == "9" {
-		return "34", nil
-	} else {
-		return "", fmt.Errorf("feature not implemented :)")
+	n, err:= strconv.Atoi(i)
+	if err != nil {
+		return "", fmt.Errorf("error in parsing the input. (err: %v)", err)
 	}
+	x, y := 0, 1
+	for j := 0; j <= n; j++{
+		x, y = y, x + y
+	}
+	return strconv.Itoa(x), nil
 }
